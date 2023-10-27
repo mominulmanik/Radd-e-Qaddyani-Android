f633a736311d08be7838b21c219fd1f617200b6b
mominulmanik <mominulmanikcse13@gmail.com>
2023-10-27 15:52:16 +0600

    new 27 oct 2023

--- app/src/main/java/com/example/radd_e_qadyyani/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/radd_e_qadyyani/Fragments/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.radd_e_qadyyani.Fragments
 
+import android.annotation.SuppressLint
 import android.content.res.AssetManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,6 +34,7 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mainTopicList = Constant().mainTopics
---
+++ app/src/main/java/com/example/radd_e_qadyyani/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.radd_e_qadyyani
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+    }
+}
